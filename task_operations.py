3546620297eba41fa44c088010e05b24ecb285fc
Bryan31793 <bryanrmzlop@gmail.com>
2026-01-18 22:44:39 -0600

    new methods: update and delete

--- task_operations.py
+++ task_operations.py
@@ -27,9 +27,42 @@ class TaskOperations():
             json.dump(tasks, file, indent=4)    #writes the list into the json file
 
     #update a task
-    def update() -> None:
-        pass
+    def update(title: str, new_title: str = None, new_description: str = None) -> None:
+
+        if not new_title and not new_description:
+            print("Exeption")
+            return 
+        
+        file_path = Path('tasks.json')
+
+        if file_path.exists():
+            with open('tasks.json', 'r') as file:
+                tasks = json.load(file)
+            for t in tasks:
+                if t['Title'] == title:
+                    if new_title:
+                        t['Title'] = new_title
+                    if new_description:
+                        t['Description'] = new_description
+
+                    with open('tasks.json', 'w') as file:
+                        json.dump(tasks, file, indent=4)
+
+                    return
+        else:
+            print("Exeption")   #usar excepcion
 
     #delete a task
-    def delete() -> None:
-        pass
+    def delete(title: str) -> None:
+        file_path = Path('tasks.json')
+
+        if file_path.exists():
+            with open('tasks.json', 'r') as file:
+                tasks = json.load(file)
+            
+            new_tasks = []
+            for t in tasks:
+                if t['title'] != title:
+                    new_tasks.append(t)
+        else:
+            print("Exeption")   #usar excepcion
